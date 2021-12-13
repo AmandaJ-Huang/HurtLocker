7d4a24d6f7f1d0f7a1ddb4d3efa639da34841743
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-13 09:46:55 -0500

    added output-test.txt; formatted text with Pattern and Matcher; wrote to output test using FileWriter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
 import org.apache.commons.io.IOUtils;
+
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+
+    private Map<String, Integer> groceryMap = new HashMap<>();
 
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
@@ -9,8 +17,26 @@ public class Main {
         return result;
     }
 
+    public static String hurtLocker(String input) {
+        Pattern delimiters = Pattern.compile("[:;^@%*]");
+        Matcher dataMatcher = delimiters.matcher(input);
+
+        String matched = dataMatcher.replaceAll("\n");
+
+        try {
+            FileWriter writer = new FileWriter("output-test.txt");
+            writer.write(matched);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
+        hurtLocker(output);
         System.out.println(output);
     }
 }
