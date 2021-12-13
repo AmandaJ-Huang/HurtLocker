8003f08c22426e722b69a095072a723242eb1b7d
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-13 13:29:44 -0500

    fixing regex

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,32 +9,42 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-    private Map<String, Map<String, Integer>> outerMap = new HashMap<>();
-    private Map<String, Integer> innerMap = new HashMap<>();
+
 
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
         return IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
     }
 
+    /** TODO: regex for all pattern types
+     * name = [nN].+[eE]:{1}([MmBbCcAa][a-zA-Z0-9]+[KkDdSs])[:;]|[nN].+[eE]:;
+     * price = [Pp][Rr].+[Ee][:;][0-9]\.[0-9]{2}
+     * type = [Tt].+[Ee][:;][Ff].+[Dd]
+     * expiration (groups date and ##) = ([0-9]+\/[0-9]+\/[0-9]+)([##$]+)
+     **/
     public static void hurtLocker(String input) {
-        Pattern delimiters = Pattern.compile("[:;^@%*]");
-        Matcher dataMatcher = delimiters.matcher(input);
+        Pattern name = Pattern.compile("");
+        Pattern price = Pattern.compile("");
+        Pattern type = Pattern.compile("");
+        Pattern expiration = Pattern.compile("");
+        Matcher dataMatcher = name.matcher(input);
+        String input1 = dataMatcher.replaceAll("\n");
 
-        /** TODO: regex for all pattern types
-         * regex for each field: milk, bread, cookies, apples
-         * ex. m[...]k --> change to Milk
-         * regex for each key: name, price, type, expiration
-         * ex. n[...]e --> change to name
-         * foods may have multiple prices
-         * regex for expiration ending with ##$ - maybe?
-         * handle null values somehow...there is at least one for each key, except expiration...
-        **/
-        String matched = dataMatcher.replaceAll("\n");
+        // food and price patterns
 
+
+        // hashmaps to hold food / price keys and occurrences
+        Map<String, Map<String, Integer>> outerMap = new HashMap<>();
+        Map<String, Integer> innerMap = new HashMap<>();
+        Map<String, Integer> outerKeySeen = new HashMap<>();
+
+
+        // put values into hashmaps
+
+        // write string to output-test file
         try {
             FileWriter writer = new FileWriter("output-test.txt");
-            writer.write(matched);
+            writer.write(input1);
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
