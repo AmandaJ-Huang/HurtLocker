327cc1d7eb03ac82dd2c0553eb51d92784e55360
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-13 14:20:55 -0500

    getting somewhere...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,32 +18,26 @@ public class Main {
 
     /** TODO: regex for all pattern types
      * all together now...
-     * name - (([nN].+[eE]:{1})([MmBbCcAa][a-zA-Z0-9]+[KkDdSs])([:;])|([nN].+[mM][eE])(:;))
+     * milkNotNull - ([Nn]..[Ee]:)([Mm]..[Kk])([:;])([Pp]...[Ee]:)([0-9]\.[0-9].)([:;])([Tt]..[Ee][:;])([Ff]..[Dd])([:;$^!*])(expiration:)([0-9]\/[0-9]+\/[0-9]...)([##$]+)
      * price - (([Pp][Rr].+[Cc][Ee])(:)([0-9]\.[0-9]{2})(;)|([Pp][Rr].+[Cc][Ee][:;]+))
      * type - (([Tt].+[Ee][:;])([Ff].+[Dd])([;^:%*]))
      * expiration - ((expiration)(:)([0-9]+\/[0-9]+\/[0-9]+)([##$]+))
      **/
     public static void hurtLocker(String input) {
-        String namePattern = "(([nN].+[eE]:{1})([MmBbCcAa][a-zA-Z0-9]+[KkDdSs])([:;])|([nN].+[mM][eE])(:;))";
-        String pricePattern = "(([Pp][Rr].+[Cc][Ee])(:)([0-9]\\.[0-9]{2})(;)|([Pp][Rr].+[Cc][Ee][:;]+))";
-        String typePattern = "(([Tt].+[Ee][:;])([Ff].+[Dd])([;^:%*]))";
-        String expirationPattern = "((expiration)(:)([0-9]+\\/[0-9]+\\/[0-9]+)([##$]+))";
-        String allTogether = namePattern + pricePattern + typePattern + expirationPattern;
-
-        Pattern fullPattern = Pattern.compile(allTogether);
-        Matcher dataMatcher = fullPattern.matcher(input);
+        String milk = "([Nn]..[Ee]:)([Mm]..[Kk])([:;])([Pp]...[Ee]:)(([0-9]\\.[0-9].)|)([:;])([Tt]..[Ee][:;])([Ff]..[Dd])([:;$^!*])(expiration:)([0-9]\\/[0-9]+\\/[0-9]...)([##$]+)";
+        Pattern milkPattern = Pattern.compile(milk);
+        Matcher milkMatcher = milkPattern.matcher(input);
 
         // hashmaps to hold food / price keys and occurrences
         Map<String, Map<String, Integer>> outerMap = new HashMap<>();
         Map<Matcher, Integer> innerMap = new HashMap<>();
         Map<String, Integer> outerKeySeen = new HashMap<>();
 
-        Matcher name = Pattern.compile(namePattern).matcher(input);
-
-
+        int counter = 0;
         // put values into hashmaps
-        while (name.find()) {
-            System.out.println(name.group(1) + "\n");
+        while (milkMatcher.find()) {
+            counter++;
+            System.out.println("milk, " + counter + ", price" + milkMatcher.group(5));
         }
 
         // write string to output-test file
